99112e424bf5735fe184d205b8fcb3d6565e39dc
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-01-31 22:18:14 +0500

    Add custom throttler and some other things

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaModule } from './common/prisma/prisma.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { GqlThrottlerGuard } from './common/guard/gqlThrottler.guard';
 
 @Module({
   imports: [
@@ -14,6 +17,12 @@ import { PrismaModule } from './common/prisma/prisma.module';
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
     }),
     PrismaModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 10,
+      },
+    ]),
     GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
       imports: [ConfigModule],
@@ -23,10 +32,18 @@ import { PrismaModule } from './common/prisma/prisma.module';
         debug: configService.get<string>('NODE_ENV') === 'development',
         playground: configService.get<string>('NODE_ENV') === 'development',
         introspection: true,
+        context: ({ req, res }) => ({ req, res }),
       }),
     }),
   ],
   controllers: [],
-  providers: [AppService, AppResolver],
+  providers: [
+    AppService,
+    AppResolver,
+    {
+      provide: APP_GUARD,
+      useClass: GqlThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
+
+  app.use(helmet());
+  app.use(cookieParser());
+  app.enableCors({ credentials: true, origin: 'http://localhost:5173' });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,9 +22,6 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-
-  app.use(cookieParser());
-  app.enableCors({ credentials: true, origin: 'http://localhost:5173' });
 
   await app.listen(port);
   console.log(`🚀 Server is running on http://localhost:${port} 🚀`);
