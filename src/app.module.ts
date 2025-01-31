a2d464b54af2fe49df631613d9c2ae2fdf83dee3
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-01-31 21:31:20 +0500

    Setup prismaService, env config and few other things

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppResolver } from './app.resolver';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        debug: configService.get<string>('NODE_ENV') === 'development',
+        playground: configService.get<string>('NODE_ENV') === 'development',
+        introspection: true,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [AppService, AppResolver],
 })
