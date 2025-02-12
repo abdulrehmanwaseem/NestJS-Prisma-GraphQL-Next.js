2daac2df3e65670d71b17488171586c19d5541d8
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-02-12 19:34:37 +0500

    Refactor redux api config & adding cookies with every subsequent reqs

--- frontend_Next.js/src/redux/api/index.ts
+++ frontend_Next.js/src/redux/api/index.ts
@@ -1,17 +1,21 @@
 import { graphqlRequestBaseQuery } from "@rtk-query/graphql-request-base-query";
 import { createApi } from "@reduxjs/toolkit/query/react";
+import { GraphQLClient } from "graphql-request";
+
+// Partial<ClientError & { errorCode: number }
+const client = new GraphQLClient("http://localhost:4000/graphql", {
+  credentials: "include",
+});
 
 export const api = createApi({
   reducerPath: "api",
   baseQuery: graphqlRequestBaseQuery({
-    url: "http://localhost:4000/graphql",
-    fetchOptions: {
-      credentials: "include",
-    },
-    customErrors: (error) => {
-      return error.response.errors;
-    },
+    client,
+    // customErrors: (error) => {
+    //   return error?.response?.errors[0];
+    // },
   }),
+  refetchOnMountOrArgChange: true,
 
   tagTypes: ["Users"],
   endpoints: () => ({}),
